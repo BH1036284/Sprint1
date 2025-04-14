6cfd918bf412197be28f05dc030a45302a0d973f
BH1036284 <bh1036284@student.francistuttle.edu>
2025-04-14 10:44:18 -0500

    Sprint 2 - Functional Icon

--- BlazorSprint1/obj/Debug/net8.0/BlazorSprint1.AssemblyInfo.cs
+++ BlazorSprint1/obj/Debug/net8.0/BlazorSprint1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSprint1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f23114c4a11d5f1f5198f74218c29555ab80ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSprint1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSprint1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
